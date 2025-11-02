256b6d0f12d7cb0768f040858a3468087e37134f
Stano Vojtko <stano.vojtko@gmail.com>
2025-11-02 21:09:17 +0100

    New Test Piece

---
+++ pieces/ZSVTest1Piece/models.py
@@ -0,0 +1,27 @@
+from pydantic import BaseModel, Field
+
+
+class InputModel(BaseModel):
+    """
+    Test Data Piece Input Model
+    """
+
+    test_input_file: str = Field(
+        title="Test input data",
+        # default='/home/shared_storage/TestData.csv',
+        description="The path to the Test Data",
+    )
+
+
+class OutputModel(BaseModel):
+    """
+    Test Data Piece Output Model
+    """
+    message: str = Field(
+        default="",
+        description="Output message to log"
+    )
+
+    file_path: str = Field(
+        description="The path to the copied Test Data file by the specified Location"
+    )
---
+++ pieces/ZSVTest1Piece/piece.py
@@ -0,0 +1,33 @@
+from ...base_piece import BasePiece
+try:
+    # prefer relative import when running as a package
+    from .models import InputModel, OutputModel
+except ImportError:
+    # fallback to absolute import for editors or alternate import paths
+    from domino_pieces.pieces.ZSVTest1Piece.models import InputModel, OutputModel
+import pandas as pd
+from pathlib import Path
+
+
+class ZSVTestPiece(BasePiece):
+    
+    def piece_function(self, input_data: InputModel):
+        
+        df_testdata = pd.read_csv(input_data.testdata_input_file)
+    
+
+        message = f"testdata for location: {input_data.location} readed successfully"
+        file_path = str(Path(self.results_path) / "Test_Data_copied.csv")
+        df_testdata.to_csv(file_path, index=False)
+
+        # Set display result
+        self.display_result = {
+            "file_type": "csv",
+            "file_path": file_path
+        }
+
+        # Return output
+        return OutputModel(
+            message=message,
+            file_path=file_path
+        )
